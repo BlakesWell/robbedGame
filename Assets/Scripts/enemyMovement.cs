78c9539a6c43355bb2cac3f2ce0b3d739e369f22
BlakesWell <blakeyblazer@gmail.com>
2025-10-21 14:35:32 -0500

    work on making enemy walk in a loop

--- Assets/Scripts/enemyMovement.cs
+++ Assets/Scripts/enemyMovement.cs
@@ -1,25 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class enemyMovement : MonoBehaviour
 {
     public Transform player;
-    public GameObject path;
-    private bool seen = false;
+    public bool seen = false;
     private NavMeshAgent nma;
+    public float distance;
+
+    [SerializeField] float waitTimeOnWayPoint = 1f;
+    [SerializeField] Path path;
+
+    float time = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         nma = GetComponent<NavMeshAgent>();
+        nma.destination = path.GetCurrentWayPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player != null && !seen)
+        distance = Vector3.Distance(this.transform.position, player.position);
+
+        if(distance < 30 && !seen)
         {
+            seen = true;
             nma.SetDestination(player.position);
         }
+        else
+        {
+            if(nma.remainingDistance <= 0.1f)
+            {
+                time += Time.deltaTime;
+                if(time >= waitTimeOnWayPoint)
+                {
+                    time = 0f;
+                    nma.destination = path.GetNextWaypoint();
+                }
+            }
+        }
     }
+
+    void OnTriggerEnter(Collider maybePlayer)
+    {
+        if(maybePlayer.gameObject.CompareTag("player")) //if player touches this block
+        {
+            print("should kill player");
+            SceneManager.LoadScene(0);
+        }
+    }
+
 }
---
+++ Assets/Scripts/path.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public enum PathType
+    {
+        Loop,
+        ReverseWhenComplete
+    }
+
+    public Transform[] waypoints;
+    public PathType pathType = PathType.Loop;
+
+    private int direction = 1;
+    int index;
+
+    public Vector3 GetCurrentWayPoint()
+    {
+        return waypoints[index].position;
+    }
+    public Vector3 GetNextWaypoint()
+    {
+        if (waypoints.Length == 0) return transform.position;
+
+        index = GetNextWaypointIndex();
+        Vector3 nextWaypoint = waypoints[index].position;
+
+        return nextWaypoint;
+    }
+
+    private int GetNextWaypointIndex()
+    {
+        index += direction;
+
+        if(pathType == Pathtype.Loop)
+        {
+            index %= waypoints.Length;
+        }
+        else if(pathType == PathType.Loop)
+        {
+            if(index >= waypoints.Length || index < 0)
+            {
+                direction *= -1;
+                index += direction * 2;
+            }
+        }
+        return index;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (waypoints == null || waypoints.Length == 0) return;
+
+        Gizmos.color = Color.white;
+
+        //Draw lines between waypoints
+        for(int i = 0; i < waypoints.Length - 1; i++)
+        {
+            Gizmos.DrawLine(waypoints[i].position, waypoints[i+1].position);
+        }
+
+        //Loop back to the start if the path is a loop
+        if(pathType == PathType.Loop)
+        {
+            Gizmos.DrawLine(waypoints[waypoints.Length - 1].position, waypoints[0].position);
+        }
+
+        Gizmos.color = Color.red;
+        //Draw the waypoints as small spheres
+        foreach (Transform waypoint in waypoints)
+        {
+            Gizmos.DrawSphere(waypoint.position, 0.2f);
+        }
+    }
+
+}
