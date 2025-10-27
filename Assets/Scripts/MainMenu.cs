ce5bf241ba64a1eeef3bc5a8e5e6b4d6298d7529
BlakesWell <blakeyblazer@gmail.com>
2025-10-27 14:37:08 -0500

    cutscenes, hitboxes, questions

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Threading;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject animated;
+    async void delayAnim()
+    {
+        await System.Threading.Tasks.Task.Delay(5000);
+    }
     public void PlayGame()
     {
+        animated.GetComponent<Animator>().enabled = true;
+
+        delayAnim();
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 }
