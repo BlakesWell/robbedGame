758cc8b614f2fade752cf740c63833c3e0cc100d
BlakesWell <blakeyblazer@gmail.com>
2025-11-07 14:35:27 -0600

    lighting & intro scene

--- Assets/Scripts/moveBg.cs
+++ Assets/Scripts/moveBg.cs
@@ -3,12 +3,9 @@ using UnityEngine;
 public class moveBg : MonoBehaviour
 {
 
-    public GameObject trees;
     public float speed = 5f;         // Speed of movement
-    public float moveDistance = 10f; // How far to move before teleporting
     public GameObject originObj;
 
-    private Vector3 startPos;
     Vector3 direction = Vector3.forward;
 
     private float movedAmount = 0f;
@@ -16,12 +13,8 @@ public class moveBg : MonoBehaviour
     public GameObject treeSection1;
     public GameObject treeSection2;
     public GameObject treeSection3;
+    public GameObject road;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        startPos = originObj.transform.position;
-    }
 
     // Update is called once per frame
     void Update()
@@ -30,7 +23,9 @@ public class moveBg : MonoBehaviour
 
         movedAmount += moveThisFrame;
 
-        foreach (Transform child in trees.transform)
+        road.transform.position += direction * moveThisFrame;
+
+        foreach (Transform child in this.transform)
         {
             
             child.position += direction * moveThisFrame;
@@ -39,11 +34,13 @@ public class moveBg : MonoBehaviour
             {
                 Debug.Log("1 should tp back");
                 treeSection1.transform.position = new Vector3(0,10,-550);
+                
             }
             else if(treeSection2.transform.position.z >= 250)
             {
                 Debug.Log("2 should tp back");
                 treeSection2.transform.position = new Vector3(0,10,-400);
+                road.transform.position = new Vector3(-165,1,-163);
             }
             else if(treeSection3.transform.position.z >= 515)
             {
