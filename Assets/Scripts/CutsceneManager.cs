d58c135dadf47b72fa28e3cebe86648c55a07408
BlakesWell <blakeyblazer@gmail.com>
2025-11-19 14:34:40 -0600

    animations and cutscene

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -3,45 +3,42 @@ using UnityEngine;
 public class CutsceneManager : MonoBehaviour
 {
     public GameObject player;
+    public GameObject animPlayer;
     public GameObject enemy;
+    public GameObject animEnemy;
     public GameObject cam;
 
-    async void playAnim()
+    System.Collections.IEnumerator playAnim()
     {
+        //yield return new WaitForSeconds(1f);
         enemy.GetComponent<enemyMovement>().enabled = false;
         player.GetComponent<PlayerController>().enabled = false;
-        player.GetComponent<Animator>().SetInteger("Mode", 2);
-        
+        //player.GetComponent<Animator>().enabled = true;
+        animPlayer.GetComponent<Animator>().SetInteger("Mode", 2);
+        animEnemy.GetComponent<Animator>().SetInteger("Mode", 2);
 
-        await System.Threading.Tasks.Task.Delay(3000);
-
+        yield return new WaitForSeconds(3f);
 
         enemy.GetComponent<Animator>().enabled = true;
-        player.GetComponent<Animator>().SetInteger("Mode", 0);
-        enemy.GetComponent<Animator>().SetInteger("Mode", 2);
+        animPlayer.GetComponent<Animator>().SetInteger("Mode", 0);
+        animEnemy.GetComponent<Animator>().SetInteger("Mode", 2);
 
-
-        await System.Threading.Tasks.Task.Delay(4000);
-
+        yield return new WaitForSeconds(4f);
 
         player.GetComponent<PlayerController>().enabled = true;
+        player.GetComponent<Animator>().enabled = false;
         enemy.GetComponent<enemyMovement>().enabled = true;
-        player.GetComponent<Animator>().SetInteger("Mode", 0);
-        enemy.GetComponent<Animator>().SetInteger("Mode", 0);
+        enemy.GetComponent<Animator>().enabled = false;
+        animPlayer.GetComponent<Animator>().SetInteger("Mode", 0);
+        animEnemy.GetComponent<Animator>().SetInteger("Mode", 0);
 
         this.GetComponent<Camera>().enabled = false;
         cam.GetComponent<Camera>().enabled = true;
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playAnim();
+        StartCoroutine(playAnim());
     }
-
-    // Update is called once per frame
-    void Update()
-    {
         
-    }
+    
 }
